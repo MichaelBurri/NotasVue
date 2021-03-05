f1b0cd37d011399e92d8bf3a264a49df214402e6
MichaelBurri <miguel_burri@hotmail.com>
2021-03-05 22:56:45 +0100

    fin

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-
+git push -f https://github.com/MichaelBurri/NotasVue.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:michaelburri/NotasVue.git master:gh-pages
+# git push -f git@github.com:michaelburri/NotasVue.git master:gh-pages
 
 cd -
